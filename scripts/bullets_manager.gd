6d00df483cac5e242a6cfc7339cd79f8a5d6366a
Bruno Griveau <brunog90@gmail.com>
2025-10-23 23:55:14 +0200

    added timer to bullet shots

--- scripts/bullets_manager.gd
+++ scripts/bullets_manager.gd
@@ -1,13 +1,22 @@
 class_name BulletsManager extends Node
 
 var bulletScene: PackedScene
+var canShoot: bool = true
+var bulletDelay: float = 0.3
 @onready var bullet_timer: Timer = $BulletTimer
 
 func _init() -> void:
 	bulletScene = preload("res://scenes/bullet.tscn")
 
 func shootBullet(playerPos: Vector2) -> void:
-	var bulletInstance = bulletScene.instantiate()
-	bulletInstance.position = Vector2(playerPos.x + 100, playerPos.y + 50)
-	add_child(bulletInstance)
+	if canShoot:
+		canShoot = false
+		var bulletInstance = bulletScene.instantiate()
+		bulletInstance.position = Vector2(playerPos.x + 100, playerPos.y + 50)
+		add_child(bulletInstance)
+		bullet_timer.start(bulletDelay)
 	
+
+
+func _on_bullet_timer_timeout() -> void:
+	canShoot = true
